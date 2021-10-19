b47818edd1d8ae2a824fe7c75cfa4daf152f3bec
pooya98 <pooya98@naver.com>
2021-10-19 23:32:25 +0900

    파이어베이스 연결 & 연결 테스트

---
+++ app/src/main/java/com/example/android_chatting/ChatActivity.java
@@ -0,0 +1,22 @@
+package com.example.android_chatting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+    }
+}
